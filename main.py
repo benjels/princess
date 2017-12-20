0b9ab925b6cfa3f69cf062822afd652330c370d5
benjels <maxbrown0101@gmail.com>
2017-12-20 18:18:18 +1300

    debugging session object

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import robobrowser
 import re
 import requests
 import configparser
+import datetime
+
 
 password = "Zoolander69"
 phone = "64274699089"
 
+
+def log(msg):
+    print ('[' + str(datetime.datetime.now()) + ']' + ' ' + msg)
 
 
 def get_access_token(email, password, ua, url):
@@ -36,7 +41,9 @@ def main():
     try:
         log("Trying to start Tinder session.")
         session = pynder.Session(str(config['DEFAULT']['FACEBOOK_ID']), auth)
+        print("debug 1")
     except pynder.errors.RequestError:
+        print("debug 2")
         log("Pynder Error. Trying to get new auth.")
         auth = get_access_token(str(config['DEFAULT']['FB_EMAIL']), str(config['DEFAULT']['FB_PASSWORD']), str(config['DEFAULT']['MOBILE_USER_AGENT']), str(config['DEFAULT']['FB_AUTH']))
         config['DEFAULT']['FACEBOOK_AUTH_TOKEN'] = auth
@@ -49,6 +56,11 @@ def main():
             log("Pynder Error. New auth did NOT work.")
             quit()
 
+    #DO SOMETHING ON TINDER YA KNOW
+    print("beep")
+    print(session)
+    print("boop")
+    
 
 
 
