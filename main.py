317ad237a95d3eafc02a4ac045d55c6f56f90661
benjels <maxbrown0101@gmail.com>
2017-12-20 17:49:10 +1300

    added some stuff for trying to get fb auth token etc

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
+import pynder
+import robobrowser
+import re
+import requests
+import configparser
+
+password = "Zoolander69"
+phone = "64274699089"
 
 
 
-
-
-
-
-
-password = coolpassword666
-
+def get_access_token(email, password, ua, url):
+    s = robobrowser.RoboBrowser(user_agent = ua, parser = "lxml")
+    s.open(url)
+    ##submit login form
+    f = s.get_form()
+    f["pass"] = password
+    f["email"] = email
+    s.submit_form(f)
+    ##click OK button on the dialog informing you that you have already authenticated with the tinder AppleWebKit
+    f = s.get_form()
+    s.submit_form(f, submit = f.submit_fields['__CONFIRM__'])
+    #get access token from the http response
+    access_token = re.search(r"access_token=([\w\d]+)", s.response.content.decode()).groups()[0]
+    return access_token
 
 
 def main():
-    print ("let's begin...")
+    print("let's begin ok")
+    requests.packages.urllib3.disable_warnings()  # Find way around this...
+    config = configparser.ConfigParser(interpolation=None)
+    config.read('config.ini')
+    auth = str(config['DEFAULT']['FACEBOOK_AUTH_TOKEN'])
+
 
 
 
